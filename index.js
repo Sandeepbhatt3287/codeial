7c1dfc7e7c909a4a048c620e894a8c1ab0f7e7f0
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-04-30 19:12:10 +0530

    Setup View engine

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 // use express router
 app.use('/',require('./routes'));
 
+// set up the view engine
+
+app.set('view engine','ejs');
+
+app.set('view','./views');
+
 app.listen(port, function(err){
     if (err){
         // we are using backticks(``) for interpolation
