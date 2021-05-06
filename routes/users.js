5166283297845a7a0736954d6903aa20801b4ce8
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-05-06 21:06:28 +0530

    routering signUp and signIn

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/users_controller');
 
 router.get('/profile',userController.profile)
+
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 module.exports = router;
