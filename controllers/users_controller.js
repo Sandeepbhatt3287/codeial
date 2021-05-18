f77e42a20dad0d77fb04574266883d704fae8e33
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-05-18 20:15:02 +0530

    updating sign in and create session

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -72,28 +72,31 @@ module.exports.create = function(req,res){
 
 // sing in and create a session for the user
 module.exports.createSession = function(req,res){
-    // steps to authenticate
-    // find he user
-    User.findOne({email: req.body.email},function(err,user){
-        if(err){
-            console.log('error in finding user in signing in');
-            return
-        }
-        // handle user found
-        if(user){
-               // handle password which doesn't match
-               if(user.password != req.body.password){
-                   return res.redirect('back');
-               }
-              // handle sesson creation
-              res.cookie('user_id',user.id);
+    // // steps to authenticate
+    // // find he user
+    // User.findOne({email: req.body.email},function(err,user){
+    //     if(err){
+    //         console.log('error in finding user in signing in');
+    //         return
+    //     }
+    //     // handle user found
+    //     if(user){
+    //            // handle password which doesn't match
+    //            if(user.password != req.body.password){
+    //                return res.redirect('back');
+    //            }
+    //           // handle sesson creation
+    //           res.cookie('user_id',user.id);
 
-              return res.redirect('/users/profile');
+    //           return res.redirect('/users/profile');
 
-        }else{
-                 // handle user not found
-                 return res.redirect('back');
-        }
-    });
-       
+    //     }else{
+    //              // handle user not found
+    //              return res.redirect('back');
+    //     }
+    // });
+
+
+    //    using passport js for authentication
+    return res.redirect('/'); 
 }
