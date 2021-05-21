35d584af7d70a5dad27d24588086d1b25520367e
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-05-21 15:24:38 +0530

    Authenticate

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -30,6 +30,10 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp=function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title : "Codeial | Sign up"
     })
@@ -38,6 +42,10 @@ module.exports.signUp=function(req,res){
 //render the sign in page
 
 module.exports.signIn = function(req,res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title : "Codeial | Sign In"
     })
